80b79d92c74568264cdd989086b549ac1786d0ce
LinnartSF <linnart.felkl@hotmail.com>
2022-10-31 23:24:18 +0100

    80% reduction

---
+++ ex_numba_02.py
@@ -0,0 +1,32 @@
+from numba import jit
+
+import random
+import time
+
+def rand_events(n: int) -> list:
+    ls = []
+    for i in range(n):
+        x = random.random()
+        while True:
+            y = random.random()
+            if x < y: 
+                ls.append(y)
+                break
+    return ls
+
+""" RUNTIME TEST WITHOUT NUMBA  -------------------------- """
+
+_start_time = time.time()
+vals = rand_events(10000000)
+_end_time = time.time()
+
+print(str(_end_time - _start_time))
+
+""" RUNTIME TEST WITH NUMBA ------------------------------ """
+
+_start_time = time.time()
+jit_rand_events = jit()(rand_events)
+vals = jit_rand_events(10000000)
+_end_time = time.time()
+
+print(str(_end_time-_start_time))
