df02282cf0a2fb2b3bee00f2cff6764fc9450057
LinnartSF <linnart.felkl@hotmail.com>
2022-10-31 23:16:55 +0100

    40% reduction

---
+++ ex_numba_01.py
@@ -0,0 +1,27 @@
+from numba import jit
+
+import random
+import time
+
+def get_heads(n: int) -> list:
+    ls = []
+    for i in range(n):
+        if random.random() < 0.5: ls.append(i)
+    return ls
+
+""" RUNTIME TEST WITHOUT NUMBA  -------------------------- """
+
+_start_time = time.time()
+vals = get_heads(10000000)
+_end_time = time.time()
+
+print(str(_end_time - _start_time))
+
+""" RUNTIME TEST WITH NUMBA ------------------------------ """
+
+_start_time = time.time()
+jit_get_heads = jit()(get_heads)
+vals = jit_get_heads(10000000)
+_end_time = time.time()
+
+print(str(_end_time-_start_time))
