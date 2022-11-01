d86204217b6e604add3d8d5ef5ffee3926dbda4e
LinnartSF <linnart.felkl@hotmail.com>
2022-11-01 01:11:15 +0100

    np and numba

---
+++ ex_numba_03.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from numba import jit
+
+import random
+import time
+
+def rand_events(n: int) -> list:
+    ls = np.zeros(n)
+    for i in range(n):
+        x = random.random()
+        while True:
+            y = random.random()
+            if x < y: 
+                ls[i] = y
+                break
+    return ls
+
+""" RUNTIME TEST WITHOUT NUMBA  -------------------------- """
+
+_start_time = time.time()
+vals = rand_events(10000000)
+_end_time = time.time()
+
+print(str(_end_time - _start_time))
+
+""" RUNTIME TEST WITH NUMBA ------------------------------ """
+
+_start_time = time.time()
+jit_rand_events = jit(nopython=True)(rand_events)
+vals = jit_rand_events(10000000)
+_end_time = time.time()
+
+print(str(_end_time-_start_time))
+
+_start_time = time.time()
+vals = jit_rand_events(10000000)
+_end_time = time.time()
+
+print(str(_end_time-_start_time))
