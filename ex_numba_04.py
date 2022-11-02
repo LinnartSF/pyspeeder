64ff785d73d3a2d7a8e6442cbcd4469c4ea373da
LinnartSF <linnart.felkl@hotmail.com>
2022-11-02 02:55:27 +0100

    .

--- ex_numba_04.py
+++ ex_numba_04.py
@@ -16,16 +16,16 @@ class Agent:
         self.attribute2 = val2
 
 # implement the agent-based simulation grid
-grid_agent = [[None for i in range(10)] for j in range(10)]  # grid contains None or agent instance references (custom datatype)
-grid_id = [[0 for i in range(10)] for j in range(10)]        # grid will only contain integers, either 0 or the id of an agent
+grid_agent = [[None for i in range(100)] for j in range(100)]  # grid contains None or agent instance references (custom datatype)
+grid_id = [[0 for i in range(100)] for j in range(100)]        # grid will only contain integers, either 0 or the id of an agent
 
 # populate the grid with some agents and maintain the IDs in a dictionary
 
 agentdict = {}
 id = 0
 
-for i in range(10):
-    for j in range(10):
+for i in range(100):
+    for j in range(100):
         if random.random() < 0.5: 
             id += 1
             agent = Agent(random.random(), random.random())
@@ -35,30 +35,32 @@ for i in range(10):
 
 # --- NEIGHBOURHOOD SEARCH WITHOUT NUMBA -------------------------
 
-def get_neighbourhood(row: int, col: int, radius: int, grid: list) -> list:
+def search_neighbourhood(row: int, col: int, radius: int, grid: list, iterations: int) -> None:
+
     row_min = row-radius
     row_max = row+radius
     col_min = col-radius
     col_max = col+radius
 
-    returnls = []
+    for _ in range(iterations): # adding iterations just to consume more time to see impact of numba later
 
-    if row_min < 1: row_min = 1
-    if row_max > 10: row_max = 10
-    if col_min < 1: col_min = 1
-    if col_max > 10: col_max = 10
+        returnls = []
 
-    for i in range(row_min,row_max+1):
-        for j in range(col_min,col_max+1):
-            if i == row and col == col:
-                pass
-            elif grid[i-1][j-1]:
-                returnls.append(grid[i-1][j-1])
+        if row_min < 1: row_min = 1
+        if row_max > 100: row_max = 100
+        if col_min < 1: col_min = 1
+        if col_max > 100: col_max = 100
 
-    return returnls
+        for i in range(row_min,row_max+1):
+            for j in range(col_min,col_max+1):
+                if i == row and col == col:
+                    pass
+                elif grid[i-1][j-1]:
+                    returnls.append(grid[i-1][j-1])
+
 
 starttime = time.time()
-neighbours = get_neighbourhood(5,5,3,grid_agent)
+neighbours = get_neighbourhood(5,5,20,grid_agent)
 endtime = time.time()
 print("neighbours: ")
 print(neighbours)
